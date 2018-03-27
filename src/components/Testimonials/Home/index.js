8816ef804b66c2c36332cbf64ad46ba817fe6d4d
Tom Hudson <tomist@gmail.com>
2018-03-27 16:25:05 -0500

    Updated testimonials

--- src/components/Testimonials/Home/index.js
+++ src/components/Testimonials/Home/index.js
@@ -14,35 +14,35 @@ const HomeTestimonials = props => {
       <Hx className={ title } color={ offBlack } size="2">Join a community of IT Pros sharpening their skills the fun way.</Hx>
       <Carousel>
         <QuoteCard
-          title=""
-          quote="Overall the training format and tools that they provide makes them immensely helpful and an extremely good value. Add that to the constantly updated and expanding selection of topics they cover makes it a one-stop-shop for anyone wanting to learn the many aspects of the IT Industry."
-          attribution="Charles Morgan, CEO and Founder"
+          title="Training that pays for itself"
+          quote="My team is moving faster than ever before. If I tallied the consulting expense of the changes we have made on our own, ITProTV probably would have paid for itself in the first 30 days."
+          attribution="Dana Morrison, IT Director"
         />
         <QuoteCard
-          title=""
-          quote="I’ve used Lynda and Pluralsight, along with several others and none have come close to the standard that ITProTV has set when it comes to IT learning. Many times, I have found that I’ve learned more from watching ITProTV than I have from my IT classes at school."
-          attribution="Nathan Davis"
+          title="ITProTV sets the standard"
+          quote="I’ve used Lynda and Pluralsight, along with several others and none have come close to the standard that ITProTV has set when it comes to IT learning."
+          attribution="Nathan Davis, Technology Helpdesk Analyst"
         />
 
         <QuoteCard
-          title=""
+          title="Improved knowledge and understanding of everyday tasks"
           quote="There is an excellent variety of courses available. Completion of coursework has improved knowledge and understanding of the tasks my staff perform each day, and I couldn’t be happier."
           attribution="Jim Ford, VP of Information Technology"
         />
         <QuoteCard
-          title=""
-          quote="As a premium member, I had an online meeting with a career coach who guided me about which course path I should take. For each course, there are a lot of labs where we can practice with real devices, and also an exam questions simulator. If you like it, go on, the money is totally worth it."
-          attribution="Jordi Perez"
+          title="ITProTV is totally worth it"
+          quote="For each course, there are a lot of labs where we can practice with real devices, and also an exam questions simulator. If you like it, go on, the money is totally worth it."
+          attribution="Jordi Perez, Networking Professional"
         />
         <QuoteCard
-          title=""
-          quote="We are using this program to train the IT staff. This provides us with the opportunity to keep our staff informed and broaden our understanding of new concepts."
+          title="Learn anywhere, anytime"
+          quote="This is great for people to use at home or on the road. If you can’t afford to send someone to a Windows 2016 server class for a week, this is a very good test prep."
           attribution="Jim Barber, Manager of Information Systems"
         />
         <QuoteCard
-          title=""
+          title="You won’t be disappointed"
           quote="This site has helped me with two certifications and also as the supplemental material for my grad school classes. Give it a try, you won’t be disappointed."
-          attribution="Jeff Patterson"
+          attribution="Jeff Patterson, IT Professional"
         />
       </Carousel>
     </div>
