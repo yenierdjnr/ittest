e4b3c2529059f0cdaa9a8b6feb0cae6a63f1f444
Roberto Fuentes <roberto.fuentes@thirteen23.com>
2018-03-29 16:05:13 -0500

    pamphlet fix

--- src/components/Pamphlet/index.js
+++ src/components/Pamphlet/index.js
@@ -12,16 +12,18 @@ const orangePrimary = '#F6621C';
 const Pamphlet = props => {
   return (
     <section className={ styles.container }>
-      <ViewportAnimation>
-        <img className={ styles['article-img'] } src={ articleImage } width="260" height="195" />
-      </ViewportAnimation>
-      <div className={ styles.info }>
-        <h2 className={ styles.title }>The Tough Ten: 10 Hardest IT  Gigs to Hire For</h2>
-        <p className={ styles.summary }>IT hiring is a bidding war. Get this article to see what's in demand  and see what each position is worth – salary ranges included!</p>
-        <a href="http://go.itpro.tv/tough_ten" className={ styles.link }>
-          <span className={ styles.action }>get my copy</span>
-          <IconArrowRight className={ styles.arrow } fill={ orangePrimary }/>
-        </a>
+      <div className={ styles.content }>
+        <ViewportAnimation>
+          <img className={ styles['article-img'] } src={ articleImage } width="260" height="195" />
+        </ViewportAnimation>
+        <div className={ styles.info }>
+          <h2 className={ styles.title }>The Tough Ten: 10 Hardest IT  Gigs to Hire For</h2>
+          <p className={ styles.summary }>IT hiring is a bidding war. Get this article to see what's in demand  and see what each position is worth – salary ranges included!</p>
+          <a href="http://go.itpro.tv/tough_ten" className={ styles.link }>
+            <span className={ styles.action }>get my copy</span>
+            <IconArrowRight className={ styles.arrow } fill={ orangePrimary }/>
+          </a>
+        </div>
       </div>
     </section>
   );
