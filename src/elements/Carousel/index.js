14e8ccd6aa1ff83b3da9a5f9d3db78f515ca09ce
Daniel Tijerina <dtijerina@twyla.com>
2018-03-30 19:51:56 -0500

    small phone

--- src/elements/Carousel/index.js
+++ src/elements/Carousel/index.js
@@ -3,12 +3,12 @@ import React, { PureComponent } from 'react';
 import Hx from 'Elements/Hx';
 import QuoteCard from 'Components/QuoteCard';
 import {
-  carousel, lastChild
+  carousel, lastChild, wrapper
 } from './styles.module.scss';
 
 // TODO: make these into props with defaults
-const DURATION_MS = 7000;
-const TRANISITION_MS = 500;
+const DURATION_MS = 8000;
+const TRANISITION_MS = 700;
 
 export default class Carousel extends PureComponent {
   constructor(props) {
@@ -55,13 +55,15 @@ export default class Carousel extends PureComponent {
       transition: !animating ? 'none' : `transform ${TRANISITION_MS}ms ease-in-out`
     }
     return (
-      <div className={ carousel } style={ style }>
-        {React.cloneElement(lastChild, { style:{ position: 'absolute', transform: 'translateX(-100%)' } })}
-        {children.map((child, index) => (
-          React.cloneElement(child, { key: index })
-        ))}
-        {(page + 2 > pages) && React.cloneElement(children[0])}
-        {(page + 3 > pages) && React.cloneElement(children[1])}
+      <div className={ wrapper }>
+        <div className={ carousel } style={ style }>
+          {React.cloneElement(lastChild, { style:{ position: 'absolute', transform: 'translateX(-100%)' } })}
+          {children.map((child, index) => (
+            React.cloneElement(child, { key: index })
+          ))}
+          {(page + 2 > pages) && React.cloneElement(children[0])}
+          {(page + 3 > pages) && React.cloneElement(children[1])}
+        </div>
       </div>
     );
   }
