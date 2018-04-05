87d4040f37f102f1cfaa7f3d7ac747ac4a013d5c
Erin McCaffrey <>
2018-04-05 16:59:59 -0400

    updates hero for career-resources

---
+++ src/components/Hero/CareerResources/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import Hx from 'Elements/Hx';
+import Para from 'Elements/Para';
+import styles from './styles.module.scss';
+
+
+const CareerResourcesHero = ({ className='' }) => {
+  return (
+    <section className={ styles.container }>
+      <div className={ styles['intro'] }>
+        <Hx size="1" className={ styles['title'] }>IT Training That’s Effective & Entertaining</Hx>
+        <Para className={ styles['subtitle'] }>Build or expand your IT career and enjoy the journey. Access binge-worthy learning™ for every IT pro.</Para>
+        <Link to="/plans/" className={ styles['link'] }>start free trial</Link>
+      </div>
+    </section>
+  );
+};
+
+
+export default CareerResourcesHero;
