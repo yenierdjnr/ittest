2ad5fafa01e24f1832ae512cf4f352793e425170
Amanda <acheng742@gmail.com>
2018-04-06 16:03:36 -0400

    fix banner styling

--- src/components/AboutContent/index.js
+++ src/components/AboutContent/index.js
@@ -8,19 +8,7 @@ import Para from 'Elements/Para';
 const AboutContent = props => {
   return (
     <section className={ styles.container }>
-        <Para className={ styles.content } color={ styles.greyDark } >It is vital that all ITProTV content be available to all aspiring and existing IT professionals regardless of disabilities. ITPro.TV is WCAG 2.0 AA compliant. Highlights of ITProTV’s accessibility features within the service include:</Para>
-        <ul className={ styles.accessibilityStyle } color={ styles.greyDark } >
-          <li>Accessibilities Controls for Primary and Audio players</li>
-          <li>Accessibilities Controls for Live video player</li>
-          <li>Course Library tabbing</li>
-          <li>Ratings</li>
-          <li>Shortcut to download button</li>
-          <li>Tabbing shortcuts to content, to video player, and to top of navigation</li>
-          <li>Labeled carousel buttons</li>
-          <li>Language on the main html tag</li>
-          <li>Roles on the navigation bar menu</li>
-        </ul>
-        <Para className={ styles.content } color={ styles.greyDark } >If you have any questions or concerns regarding accessibility, please email support@itpro.tv.</Para>
+        <Hx className={ styles.subtitle } color={ styles.offBlack }>About ITProTV</Hx>
     </section>
   );
 };
