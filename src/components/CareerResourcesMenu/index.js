fcaeb1b00490b833163ca1e25e5727eed13544b2
Erin McCaffrey <>
2018-04-06 16:04:16 -0400

    adds all the menu icons and text links

--- src/components/CareerResourcesMenu/index.js
+++ src/components/CareerResourcesMenu/index.js
@@ -4,6 +4,9 @@ import styles from './styles.module.scss';
 import Hx from 'Elements/Hx';
 import Para from 'Elements/Para';
 import imagePaths from 'Images/career-path.svg';
+import imageCertification from 'Images/certification.svg';
+import imageResources from 'Images/resources.svg';
+import TextLink from 'Elements/TextLink';
 
 const CareerResourcesMenu = props => {
   return (
@@ -11,9 +14,21 @@ const CareerResourcesMenu = props => {
       <ul className={ styles.content }>
         <li className={ styles.item }>
           <div className={ `${styles['icon-box']} ${styles.paths}` }>
-            <img className={ styles['img-paths'] } src={ imagePaths } alt="paths" />
+            <img src={ imagePaths } alt="paths" />
           </div>
-          <Hx className={ styles.title } size="4">IT Career Paths</Hx>
+          <TextLink className={ styles['menu-link'] }>IT Career Paths</TextLink>
+        </li>
+        <li className={ styles.item }>
+          <div className={ `${styles['icon-box']} ${styles.paths}` }>
+            <img src={ imageCertification } alt="certification" />
+          </div>
+          <TextLink className={ styles['menu-link'] }>Getting IT Certified</TextLink>
+        </li>
+        <li className={ styles.item }>
+          <div className={ `${styles['icon-box']} ${styles.paths}` }>
+            <img src={ imageResources } alt="IT resources" />
+          </div>
+          <TextLink className={ styles['menu-link'] }>IT Resources</TextLink>
         </li>
       </ul>
     </section>
