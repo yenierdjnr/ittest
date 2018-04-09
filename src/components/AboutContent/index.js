c877d238844a449927f4cbb8a42d146c7da85ce5
Amanda <acheng742@gmail.com>
2018-04-09 11:45:31 -0400

    add about text content

--- src/components/AboutContent/index.js
+++ src/components/AboutContent/index.js
@@ -9,6 +9,9 @@ const AboutContent = props => {
   return (
     <section className={ styles.container }>
         <Hx className={ styles.subtitle } color={ styles.offBlack }>About ITProTV</Hx>
+        <Para className={ styles.content } color={ styles.greyDark } >ITProTV takes the best aspects of in-person training and delivers it online and on-demand. Attractive studios, engaging hosts and effective content make ITProTV the choice of IT professionals globally.</Para>
+        <Hx className={ styles.subtitle } color={ styles.offBlack }>About ITProTV</Hx>
+        <Para className={ styles.content } color={ styles.greyDark } >ITProTV takes the best aspects of in-person training and delivers it online and on-demand. Attractive studios, engaging hosts and effective content make ITProTV the choice of IT professionals globally.</Para>
     </section>
   );
 };
