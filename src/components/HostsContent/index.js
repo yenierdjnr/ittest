b9906534eb392175cd68b4d08a5ba8448c870d37
Amanda <acheng742@gmail.com>
2018-04-20 09:30:10 -0400

    try to fix spacing issue

--- src/components/HostsContent/index.js
+++ src/components/HostsContent/index.js
@@ -4,16 +4,12 @@ import styles from './styles.module.scss';
 import Hx from 'Elements/Hx';
 import Para from 'Elements/Para';
 import Tim from 'Images/tim.jpg';
-import Don from 'Images/don.jpg';
 
 
 
 const HostsContent = props => {
   return (
     <section className={ styles.container }>
-      <Hx className={ styles.subtitle } color={ styles.offBlack }>Meet the Hosts</Hx>
-
-      <div className={ styles.hostContainer }>
         <div className={ styles.img }>
           <img className={ styles.headshot } src={ Tim } alt="Tim Broom photo" />
         </div>
@@ -22,7 +18,6 @@ const HostsContent = props => {
           <Para className={ styles.title } color={ styles.offBlack }>Adam has 30+ years as an IT instructor in the private and public sectors. He holds more than 160 IT certifications. He has trained thousands of IT pros over the course of his career.</Para>
           <Para className={ styles.title } color={ styles.offBlack }>Certifications: 160+</Para>
         </div>
-      </div>
 
     </section>
   );
