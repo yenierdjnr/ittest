46544f1f1dec4defedaf7eb02e4b8fe95e6cd122
Amanda <acheng742@gmail.com>
2018-05-02 17:03:56 -0400

    add list of practice exams available

--- src/components/TestContent/index.js
+++ src/components/TestContent/index.js
@@ -11,14 +11,18 @@ const TestContent = props => {
         <Para className={ styles.content } color={ styles.greyDark } >Take and re-take practice tests so you're confident when you sit for the exam. Access practice tests from Kaplan IT Training (formerly Transcender) with your ITProTV membership.</Para>
         <Hx className={ styles.title } color={ styles.offBlack } size="4">Practice tests available for:</Hx>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Microsoft</li>
+          <li>Cisco</li>
+          <li>Microsoft Developer</li>
+          <li>EC-Council</li>
+          <li>Axelos</li>
+          <li>PMI</li>
+          <li>VMWare</li>
+          <li>GIAC</li>
+          <li>Logical Operations</li>
+          <li>ISACA</li>
+          <li>CompTIA</li>
+          <li>(ISC)2</li>
         </ul>
     </section>
   );
