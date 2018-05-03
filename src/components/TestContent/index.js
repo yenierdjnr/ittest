44a91a8e60cf6e4a202a97399f57c416803f9d62
Amanda <acheng742@gmail.com>
2018-05-03 16:59:45 -0400

    position list of exams

--- src/components/TestContent/index.js
+++ src/components/TestContent/index.js
@@ -10,20 +10,26 @@ const TestContent = props => {
     <section className={ styles.container }>
         <Para className={ styles.content } color={ styles.greyDark } >Take and re-take practice tests so you're confident when you sit for the exam. Access practice tests from Kaplan IT Training (formerly Transcender) with your ITProTV membership.</Para>
         <Hx className={ styles.title } color={ styles.offBlack } size="4">Practice tests available for:</Hx>
-        <ul>
-          <li>Microsoft</li>
-          <li>Cisco</li>
-          <li>Microsoft Developer</li>
-          <li>EC-Council</li>
-          <li>Axelos</li>
-          <li>PMI</li>
-          <li>VMWare</li>
-          <li>GIAC</li>
-          <li>Logical Operations</li>
-          <li>ISACA</li>
-          <li>CompTIA</li>
-          <li>(ISC)2</li>
-        </ul>
+        <div className={ styles.listContainer }>
+          <ul className={ styles.listStyle }>
+            <li>Microsoft</li>
+            <li>Cisco</li>
+            <li>Microsoft Developer</li>
+            <li>EC-Council</li>
+          </ul>
+          <ul className={ styles.listStyleTwo }>
+            <li>Axelos</li>
+            <li>PMI</li>
+            <li>VMWare</li>
+            <li>GIAC</li>
+          </ul>
+          <ul className={ styles.listStyleThree }>
+            <li>Logical Operations</li>
+            <li>ISACA</li>
+            <li>CompTIA</li>
+            <li>(ISC)2</li>
+          </ul>
+        </div>
     </section>
   );
 };
