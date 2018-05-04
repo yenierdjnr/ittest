0b287f02f36766d51e09a3f45f35b58c9afdff3c
Amanda <acheng742@gmail.com>
2018-05-04 11:21:31 -0400

    organize list of exams

--- src/components/TestContent/index.js
+++ src/components/TestContent/index.js
@@ -8,28 +8,28 @@ import Para from 'Elements/Para';
 const TestContent = props => {
   return (
     <section className={ styles.container }>
-        <Para className={ styles.content } color={ styles.greyDark } >Take and re-take practice tests so you're confident when you sit for the exam. Access practice tests from Kaplan IT Training (formerly Transcender) with your ITProTV membership.</Para>
+        <Para className={ styles.content } color={ styles.greyDark } >Take and re-take practice tests so you are confident when you sit for the exam. Access practice tests from Kaplan IT Training (formerly Transcender) with your ITProTV membership.</Para>
         <Hx className={ styles.title } color={ styles.offBlack } size="4">Practice tests available for:</Hx>
-        <div className={ styles.listContainer }>
-          <ul className={ styles.listStyle }>
-            <li>Microsoft</li>
-            <li>Cisco</li>
-            <li>Microsoft Developer</li>
-            <li>EC-Council</li>
-          </ul>
-          <ul className={ styles.listStyleTwo }>
-            <li>Axelos</li>
-            <li>PMI</li>
-            <li>VMWare</li>
-            <li>GIAC</li>
-          </ul>
-          <ul className={ styles.listStyleThree }>
-            <li>Logical Operations</li>
-            <li>ISACA</li>
-            <li>CompTIA</li>
-            <li>(ISC)2</li>
-          </ul>
-        </div>
+        <ul className={ styles.listContainer }>
+          <li className={ styles.item }>
+            <Para className={ styles.title } color={ styles.offBlack }>Microsoft</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>Cisco</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>CompTIA</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>EC-Council</Para>
+          </li>
+          <li className={ styles.item }>
+            <Para className={ styles.title } color={ styles.offBlack }>Axelos</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>PMI</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>VMWare</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>GIAC</Para>
+          </li>
+          <li className={ styles.item }>
+            <Para className={ styles.title } color={ styles.offBlack }>Logical Operations</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>Microsoft Developer</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>ISACA</Para>
+            <Para className={ styles.title } color={ styles.offBlack }>(ISC)2</Para>
+          </li>
+        </ul>
     </section>
   );
 };
