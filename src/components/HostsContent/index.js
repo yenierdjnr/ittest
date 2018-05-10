54dd124a6f18f49a501d4f5a653f38060f0d13cb
Amanda <acheng742@gmail.com>
2018-05-10 14:16:18 -0400

    style image and content of body

--- src/components/HostsContent/index.js
+++ src/components/HostsContent/index.js
@@ -3,20 +3,20 @@ import React from 'react';
 import styles from './styles.module.scss';
 import Hx from 'Elements/Hx';
 import Para from 'Elements/Para';
-import Tim from 'Images/tim.jpg';
+import Adam from 'Images/adam.jpg';
 
 
 
 const HostsContent = props => {
   return (
     <section className={ styles.container }>
-        <div className={ styles.img }>
-          <img className={ styles.headshot } src={ Tim } alt="Tim Broom photo" />
-        </div>
-        <div className={ styles.bio }>
-          <Para className={ styles.name } color={ styles.offBlack }>Adam Gordon</Para>
-          <Para className={ styles.title } color={ styles.offBlack }>Adam has 30+ years as an IT instructor in the private and public sectors. He holds more than 160 IT certifications. He has trained thousands of IT pros over the course of his career.</Para>
-          <Para className={ styles.title } color={ styles.offBlack }>Certifications: 160+</Para>
+        <div className={ styles.wrapper }>
+          <img className={ styles.headshot } src={ Adam } alt="Adam Gordon photo" />
+          <div className={ styles.content }>
+            <Para className={ styles.name } color={ styles.offBlack }>Adam Gordon</Para>
+            <Para className={ styles.bio } color={ styles.offBlack }>Adam has 30+ years as an IT instructor in the private and public sectors. He holds more than 160 IT certifications. He has trained thousands of IT pros over the course of his career.</Para>
+            <Para className={ styles.title } color={ styles.greyDark }>Certifications: 160+</Para>
+          </div>
         </div>
 
     </section>
