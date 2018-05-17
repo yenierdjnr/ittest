7bcb93ba9a05579649c43af0fb189a812d97783d
Erin McCaffrey <>
2018-05-17 15:54:48 -0400

    adds images to resources in it resources

--- src/elements/Flags/Resource/index.js
+++ src/elements/Flags/Resource/index.js
@@ -5,12 +5,15 @@ import Hx from 'Elements/Hx';
 import Para from 'Elements/Para';
 import TextLinkExternal from 'Elements/TextLinkExternal';
 
-const Resource = ({ className='', title, description, url }) => {
+const Resource = ({ className='', title, description, url, img }) => {
 
   return (
-    <div>
-      <Hx color={ styles.headingColor } size="6">{ title }</Hx>
-      <Para color={ styles.darkGrey } className={ styles.summary }>{ description }</Para>
+    <div className={ styles.container }>
+      <Hx size="6"><a href={ `${url}` } color={ styles.title }>{ title }</a></Hx>
+      <div className={ styles.content }>
+        <img src={ `${img}` } className={ styles.img } />
+        <Para color={ styles.darkGrey } className={ styles.summary }>{ description }</Para>
+      </div>
       <TextLinkExternal className={ styles.more } to={url}>Read more</TextLinkExternal>
     </div>
   );
