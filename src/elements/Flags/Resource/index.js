c988eb610690c74bd6aced6e1a84d5bbcd9915be
Erin McCaffrey <>
2018-05-17 16:59:53 -0400

    fixes conditional image styling

--- src/elements/Flags/Resource/index.js
+++ src/elements/Flags/Resource/index.js
@@ -5,13 +5,13 @@ import Hx from 'Elements/Hx';
 import Para from 'Elements/Para';
 import TextLinkExternal from 'Elements/TextLinkExternal';
 
-const Resource = ({ className='', title, description, url, img }) => {
+const Resource = ({ className='', title, description, url, img='' }) => {
 
   return (
     <div className={ styles.container }>
       <Hx size="6"><a href={ `${url}` } color={ styles.title }>{ title }</a></Hx>
       <div className={ styles.content }>
-        <img src={ `${img}` } className={ styles.img } />
+        { img && <img src={ `${img}` } className={ styles.img } /> }
         <Para color={ styles.darkGrey } className={ styles.summary }>{ description }</Para>
       </div>
       <TextLinkExternal className={ styles.more } to={url}>Read more</TextLinkExternal>
