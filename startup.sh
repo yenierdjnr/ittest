e937fa3c3ff1a653590d3e301968079163726609
Sara Lichtenstein <saralich@ufl.edu>
2018-05-22 17:06:19 -0400

    [ch8170] add startup script, creates compose for semaphore

---
+++ startup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+CMD=$1
+yarn global add gatsby-cli@1.1.52
+
+case "$CMD" in
+  "develop")
+    gatsby develop --verbose -H 0.0.0.0
+    ;;
+  "build")
+    gatsby build --verbose
+    ;;
+  *)
+    echo "Unknown command, Gatsby has been shot, exit 1"
+    ;;
+esac;
