b10fa27dc81db610893230862d118791214bd90c
Amanda <acheng742@gmail.com>
2018-05-29 14:41:47 -0400

    move paragraph text to hero section

--- src/components/Hero/VirtualLabs/index.js
+++ src/components/Hero/VirtualLabs/index.js
@@ -10,7 +10,8 @@ const VirtualLabsHero = ({ className='', handleSwitch }) => {
     <section className={ styles.container }>
       <div className={ styles['intro'] }>
         <Hx size="1" className={ styles['title'] }>Virtual Labs</Hx>
-        <Para className={ styles['subtitle'] }>Try your new skills in a risk-free environment</Para>
+        <Hx size="3" className={ styles['subtitle'] }>Try your new skills in a risk-free environment</Hx>
+        <Para className={ styles['subtext'] }>Get on-demand access to cloned virtual machine environments. Access hands-on labs without expensive hardware or complex set up. You can run your virtual machine labs with multiple instances of Windows Server and Desktop Clients on your OSX, Linux, iOS device and of course, Windows platform.</Para>
       </div>
     </section>
   );
