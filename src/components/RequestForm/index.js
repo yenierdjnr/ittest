3a65a6b3ac6bb07847bb7a2790192659a7c33af2
Erin McCaffrey <>
2018-06-13 10:11:05 -0400

    adds testing form for for-teams

--- src/components/RequestForm/index.js
+++ src/components/RequestForm/index.js
@@ -31,7 +31,7 @@ class RequestForm extends Component {
 
     this.setState({ submitting : true })
 
-    fetch('https://go.itpro.tv/l/425902/2018-02-28/86q1kh', {
+    fetch('https://go.itpro.tv/l/425902/2018-06-13/8dybyw', {
       method: 'POST',
       body: JSON.stringify(this.state.form),
       headers: {
