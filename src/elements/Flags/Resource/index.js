39f004552c4a906367d6394aa3cffac1a6aac358
Amanda <acheng742@gmail.com>
2018-06-14 13:54:06 -0400

    add target blank to a tags

--- src/elements/Flags/Resource/index.js
+++ src/elements/Flags/Resource/index.js
@@ -9,7 +9,7 @@ const Resource = ({ className='', title, description, url, imgSrc='' }) => {
 
   return (
     <div className={ styles.container }>
-      <Hx size="6"><a href={ `${url}` } color={ styles.title }>{ title }</a></Hx>
+      <Hx size="6"><a href={ `${url}` } color={ styles.title } target="_blank">{ title }</a></Hx>
       <div className={ styles.content }>
         { imgSrc && <img src={ `${imgSrc}` } className={ styles.img } /> }
         <Para color={ styles.darkGrey } className={ styles.summary }>{ description }</Para>
