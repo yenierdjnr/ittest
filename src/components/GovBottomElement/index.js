d5cf54e71061e6f7ee86f01f06bed03550793f04
Amanda <acheng742@gmail.com>
2018-06-15 08:52:15 -0400

    remove extra space and textlink import

--- src/components/GovBottomElement/index.js
+++ src/components/GovBottomElement/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import styles from './styles.module.scss';
 import Hx from 'Elements/Hx';
 import Para from 'Elements/Para';
-import TextLink from 'Elements/TextLink';
 import { IconLgBuilding, IconSmBuilding, IconBooks } from 'Elements/Icons';
 
 
