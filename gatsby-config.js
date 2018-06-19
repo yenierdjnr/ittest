ed0705dbb81241f6f3042a901688d5b9431f3597
Trevis <trevis@flipstone.com>
2018-06-19 13:48:00 -0400

    [ch9172] Add google-tag-manager IDs for environments

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
     resolve: `gatsby-plugin-google-tagmanager`,
     options: {
-      id: "GTM-PTHSHMZ",
+      id: `${process.env.GOOGLE_TAG_MGR_ID}`,
 
       // Include GTM in development.
       // Defaults to false meaning GTM will only be loaded in production.
