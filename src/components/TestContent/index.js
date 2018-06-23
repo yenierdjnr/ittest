de2a609afaeb6c3360c8050b25aaaf4fd713c569
Amanda <acheng742@gmail.com>
2018-06-23 16:18:06 -0400

    replace list items with divs and extend columns in stylesheet

--- src/components/TestContent/index.js
+++ src/components/TestContent/index.js
@@ -18,26 +18,26 @@ const TestContent = props => {
           <Hx className={ styles.title } color={ styles.offBlack } size="4">Practice tests available for:</Hx>
         </div>
       </div>
-      <ul className={ styles.listcapsule }>
-        <li className={ `${styles['col-md-4']} ${ styles.item }` }>
+      <div className={ styles.listcapsule }>
+        <div className={ styles.item }>
           <Para className={ styles.title } color={ styles.offBlack }>Microsoft</Para>
           <Para className={ styles.title } color={ styles.offBlack }>Cisco</Para>
           <Para className={ styles.title } color={ styles.offBlack }>CompTIA</Para>
           <Para className={ styles.title } color={ styles.offBlack }>EC-Council</Para>
-        </li>
-        <li className={ `${styles['col-md-4']} ${ styles.item }` }>
+        </div>
+        <div className={ styles.item }>
           <Para className={ styles.title } color={ styles.offBlack }>Axelos</Para>
           <Para className={ styles.title } color={ styles.offBlack }>PMI</Para>
           <Para className={ styles.title } color={ styles.offBlack }>VMware</Para>
           <Para className={ styles.title } color={ styles.offBlack }>GIAC</Para>
-        </li>
-        <li className={ `${styles['col-md-4']} ${ styles.item }` }>
+        </div>
+        <div className={ styles.item }>
           <Para className={ styles.title } color={ styles.offBlack }>Logical Operations</Para>
           <Para className={ styles.title } color={ styles.offBlack }>Microsoft Developer</Para>
           <Para className={ styles.title } color={ styles.offBlack }>ISACA</Para>
           <Para className={ styles.title } color={ styles.offBlack }>(ISC)<sup>2</sup></Para>
-        </li>
-      </ul>
+        </div>
+      </div>
     </section>
   );
 };
