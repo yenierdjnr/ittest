34865ecaf84b4970055e7aac60d24f0b77258c01
Amanda <acheng742@gmail.com>
2018-06-25 10:33:34 -0400

    remove grid and style icon section with bootstrap

--- src/components/GovernmentContent/index.js
+++ src/components/GovernmentContent/index.js
@@ -10,38 +10,34 @@ import { IconCertificate, IconLabs, IconExams } from 'Elements/Icons';
 const GovernmentContent = props => {
   return (
     <section className={ styles.capsule }>
-
-        <ul className={ styles.list }>
-          <li className={ styles.item }>
-            <div className={ styles['icon-box'] }>
-              <IconCertificate className={ styles['laptop-icon'] }/>
-            </div>
-            <div className={ styles.summary }>
-              <Hx color={ styles.offBlack } className={ styles.title } size="5">One Source For All Training</Hx>
-              <Para color={ styles.greyDark } className={ styles.subtitle }>CompTIA, Microsoft, EC-Council, Apple, Cisco, ISACA, (ISC)<sup>2</sup>, and more.</Para>
-              <a href="/courses/" target="_blank" className={ styles.link }> See all courses.</a>
-            </div>
-          </li>
-          <li className={ styles.item }>
-            <div className={ styles['icon-box'] }>
-            <IconLabs className={ styles['gear-icon'] }/>
-            </div>
-            <div className={ styles.summary }>
-              <Hx color={ styles.offBlack } className={ styles.title } size="5">Hands-on with Virtual Labs</Hx>
-              <Para color={ styles.greyDark } className={ styles.subtitle }>Try out your skills on more than 800 virtual machine labs. Configure, troubleshoot, and more in a no-risk environment.</Para>
-            </div>
-          </li>
-          <li className={ `${styles.item} ${styles.binge}` }>
-            <div className={ styles['icon-box'] }>
-              <IconExams className={ styles['popcorn-icon'] }/>
-            </div>
-            <div className={ styles.summary }>
-              <Hx color={ styles.offBlack } className={ styles.title } size="5">Access Practice Tests</Hx>
-              <Para color={ styles.greyDark } className={ styles.subtitle }>Prepare for certification exams with current tests from Kaplan IT Training, formerly Transcender&reg;.</Para>
-            </div>
-          </li>
-        </ul>
-
+      <div className={ styles.item }>
+        <div className={ styles['icon-box'] }>
+          <IconCertificate className={ styles['cert-icon'] }/>
+        </div>
+        <div className={ styles.summary }>
+          <Hx color={ styles.offBlack } className={ styles.title } size="5">One Source For All Training</Hx>
+          <Para color={ styles.greyDark } className={ styles.subtitle }>CompTIA, Microsoft, EC-Council, Apple, Cisco, ISACA, (ISC)<sup>2</sup>, and more.</Para>
+          <a href="/courses/" target="_blank" className={ styles.link }> See all courses.</a>
+        </div>
+      </div>
+      <div className={ styles.item }>
+        <div className={ styles['icon-box'] }>
+          <IconLabs className={ styles['lab-icon'] }/>
+        </div>
+        <div className={ styles.summary }>
+          <Hx color={ styles.offBlack } className={ styles.title } size="5">Hands-on with Virtual Labs</Hx>
+          <Para color={ styles.greyDark } className={ styles.subtitle }>Try out your skills on more than 800 virtual machine labs. Configure, troubleshoot, and more in a no-risk environment.</Para>
+        </div>
+      </div>
+      <div className={ `${styles.item} ${styles.binge}` }>
+        <div className={ styles['icon-box'] }>
+          <IconExams className={ styles['exam-icon'] }/>
+        </div>
+        <div className={ styles.summary }>
+          <Hx color={ styles.offBlack } className={ styles.title } size="5">Access Practice Tests</Hx>
+          <Para color={ styles.greyDark } className={ styles.subtitle }>Prepare for certification exams with current tests from Kaplan IT Training, formerly Transcender&reg;.</Para>
+        </div>
+      </div>
     </section>
   );
 };
