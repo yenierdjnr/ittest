c60eeac77f7d573ad29c10f6aa70ed6d13586004
Erin McCaffrey <>
2018-06-25 10:40:51 -0400

    revises code...hopefully looks better in IE

--- src/components/Values/index.js
+++ src/components/Values/index.js
@@ -15,46 +15,44 @@ const Values = props => {
           <Hx className={ styles.subtitle } color={ styles.offBlack }>Values</Hx>
         </div>
       </div>
-      <ul className={ styles.listcapsule }>
-        <li className={ styles.item }>
+      <div className={ styles.listcapsule }>
+        <div className={ styles.item }>
           <div className={ styles.logo }>
             <IconLightBulb />
             <Para className={ styles.value } color={ styles.offBlack }>Producing excellence through innovation, imagination, and grit.</Para>
           </div>
-        </li>
-        <li className={ styles.item }>
+        </div>
+        <div className={ styles.item }>
           <div className={ styles.logo }>
             <IconCalendar />
             <Para className={ styles.value } color={ styles.offBlack }>Creating engaging content daily.</Para>
           </div>
-        </li>
-        <li className={ styles.item }>
+        </div>
+        <div className={ styles.item }>
           <div className={ styles.logo }>
             <IconBadge />
             <Para className={ styles.value } color={ styles.offBlack }>Working with honor.</Para>
           </div>
-        </li>
-      </ul>
-      <ul className={ styles.listcapsule }>
-        <li className={ styles.item }>
+        </div>
+        <div className={ styles.item }>
           <div className={ styles.logo }>
             <IconMountains />
             <Para className={ styles.value } color={ styles.offBlack }>Willingly facing challenges with optimism.</Para>
           </div>
-        </li>
-        <li className={ styles.item }>
+        </div>
+        <div className={ styles.item }>
           <div className={ styles.logo }>
             <IconPeople />
             <Para className={ styles.value } color={ styles.offBlack }>Existing for our members.</Para>
           </div>
-        </li>
-        <li className={ styles.item }>
+        </div>
+        <div className={ styles.item }>
           <div className={ styles.logo }>
             <IconSmiley />
             <Para className={ styles.value } color={ styles.offBlack }>Exuding passion, personality, and perseverance.</Para>
           </div>
-        </li>
-      </ul>
+        </div>
+      </div>
     </section>
   );
 };
