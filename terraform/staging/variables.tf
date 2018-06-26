4f8eb87dd8c449c62951940c4d52bf0aa5b9fda2
Trevis <trevis@flipstone.com>
2018-06-26 15:31:10 -0400

    [ch9879] Adds ECR Repository

---
+++ terraform/staging/variables.tf
@@ -0,0 +1,15 @@
+variable "image_tag" {
+  type = "string"
+}
+
+variable "itprotv_marketing_repo" {
+  type = "string"
+}
+
+variable "proxy_url" {
+  type = "string"
+}
+
+variable "region" {
+  type = "string"
+}
