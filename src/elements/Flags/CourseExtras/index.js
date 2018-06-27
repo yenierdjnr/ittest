739158335e5801aad6e566d0dd54d534616cade1
Erin McCaffrey <>
2018-06-27 10:24:09 -0400

    fine tunes styling for course page

--- src/elements/Flags/CourseExtras/index.js
+++ src/elements/Flags/CourseExtras/index.js
@@ -11,14 +11,14 @@ const CourseTopic = ({ className='', vLab='', exam='' }) => {
     <div className={ styles.extras }>
       <div className={ styles.extra } color={ styles.offBlack }>
         { vLab &&
-          <div className={ styles.colXS12 }>
+          <div>
             <IconVirtualLab className={ styles.labsicon } /><span className={ styles.para }>This course has a virtual lab</span>
           </div>
         }
       </div>
       <div className={ styles.extra } color={ styles.offBlack } >
         { exam &&
-          <div className={ styles.colXS12 }>
+          <div>
             <IconPracticeExam className={ styles.examsicon } /><span className={ styles.para }>This course has a practice exam</span>
           </div>
         }
