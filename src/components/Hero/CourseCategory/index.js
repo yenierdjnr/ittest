c6a0df689c5a132b2ec82ad078ae85032b4def9e
Erin McCaffrey <>
2018-06-28 09:33:03 -0400

    removes css grid from course category page, bootstraps it up!

--- src/components/Hero/CourseCategory/index.js
+++ src/components/Hero/CourseCategory/index.js
@@ -10,15 +10,17 @@ import styles from './styles.module.scss';
 const CourseCategoryHero = props => {
   return (
     <section className={ styles.capsule }>
-      <div className={ styles.intro }>
-        <Link to="/courses/" className={ styles.navigation }>
-          <IconArrowRight fill={ styles.orangePrimary } className={ styles.arrow } />
-          Courses
-        </Link>
-        <Hx className={ styles.title }  size="1">{props.title}</Hx>
-        <Para className={ styles.summary }>
-          {props.description}
-        </Para>
+      <div className={ styles.row }>
+        <div className={ styles.intro }>
+          <Link to="/courses/" className={ styles.navigation }>
+            <IconArrowRight fill={ styles.orangePrimary } className={ styles.arrow } />
+            Courses
+          </Link>
+          <Hx className={ styles.title }  size="1">{props.title}</Hx>
+          <Para className={ styles.summary }>
+            {props.description}
+          </Para>
+        </div>
       </div>
     </section>
   );
