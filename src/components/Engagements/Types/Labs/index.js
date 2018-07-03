906404d393003c1b0211d1a27e7ff4bc18f4eecd
Erin McCaffrey <>
2018-07-03 10:55:38 -0400

    removes css grid from stats and video section on homepage, bootstraps

--- src/components/Engagements/Types/Labs/index.js
+++ src/components/Engagements/Types/Labs/index.js
@@ -14,14 +14,14 @@ class Labs extends PureComponent {
     return (
       <div className={ `${className}` }>
         <div className={ styles['info-cols'] }>
-          <div className={ `${styles['info']} ${styles['info--flip']}` }>
+          <div className={ styles.info }>
             <span className={ styles['sequence-num'] }>_02</span>
             <Hx className={ styles.title } color={ styles.offBlack }>Stay Current with New Content</Hx>
             <Para className={ `${styles.summary} ${styles.exams}` } color={ styles.darkGrey }>New content is added daily so your training is always aligned with the latest UI and the current exam. Episodes go from studio to web in 24-hours.</Para>
           </div>
         </div>
         <div className={ styles['img-cols'] }>
-          <div className={ `${styles['img-box']} ${styles['img-box--flip']}` }>
+          <div className={ styles['img-box'] }>
             <img className={ styles['img-labs'] } src={ imageLabs } alt="labs" />
           </div>
         </div>
