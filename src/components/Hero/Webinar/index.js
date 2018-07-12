7e4298fd92e57d5b1029ca98f575ac54ea324dd8
Amanda <acheng742@gmail.com>
2018-07-12 15:36:50 -0400

    add images and content to page

--- src/components/Hero/Webinar/index.js
+++ src/components/Hero/Webinar/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Hx from 'Elements/Hx';
 import Para from 'Elements/Para';
 import ButtonLink from 'Elements/ButtonLink';
+import WebinarLogo from 'Images/webinar-logo.png';
 
 import styles from './styles.module.scss';
 
@@ -10,8 +11,16 @@ import styles from './styles.module.scss';
 const WebinarHero = ({ className='' }) => {
   return (
     <section className={ styles.capsule }>
-      <div className={ styles.intro }>
-        <Hx size="1" className={ styles.title }>ITProTV Webinars</Hx>
+      <div className={ styles.container }>
+        <div className={ styles.row }>
+          <div className={ styles.subtext }>
+            <Hx size="1" color={ styles.offBlack } className={ styles.title }>ITProTV Webinars</Hx>
+            <Para color={ styles.offBlack }>Take an hour out of your day to brush up on a hot topic in IT. Join the ITProTV subject matter experts for IT Pro in the Know - the ITProTV webinar series. Engage in the live broadcast or watch on demand at your convenience.</Para>
+          </div>
+          <div className={ styles.logo }>
+            <img src={ WebinarLogo } className={ styles.imgLogo } />
+          </div>
+        </div>
       </div>
     </section>
 
