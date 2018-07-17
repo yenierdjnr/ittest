94c9fafbb72bca2861eb2bef46ae1511356f4a2f
Sara Lichtenstein <sara@itpro.tv>
2018-07-17 15:16:28 -0400

    [ch10253] Job resources bug fix

--- src/pages/career-resources/job-resources/index.js
+++ src/pages/career-resources/job-resources/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from 'Components/layout';
+import { graphql } from 'gatsby';
 
+import ITResourcesContent from 'Components/ITResourcesContent';
 import Head from 'Components/Head';
 import { ITResourcesHero as Hero } from 'Components/Hero';
 import styles from './itResources.module.scss';
 
-const JobResources = ({ location }) =>
+const JobResources = ({ data, location }) =>
   <Layout location={ location }>
     <Head location={ location } />
     <main className={ styles.Career }>
@@ -14,7 +16,7 @@ const JobResources = ({ location }) =>
       </section>
 
       <section className={ styles.Info }>
-
+        <ITResourcesContent itResources={ data.itResources.edges.map((edge) => edge.node) } />
       </section>
 
     </main>
@@ -22,3 +24,21 @@ const JobResources = ({ location }) =>
 
 
 export default JobResources;
+export const query = graphql`
+  query ITResourcesQuery {
+      itResources: allItResourcesJson(sort: {order:ASC, fields: [weight]}) {
+        edges {
+          node {
+            itresourcecategory,
+            url,
+            resources {
+              title,
+              description,
+              img,
+              url
+            }
+          }
+        }
+      }
+    }
+`;
