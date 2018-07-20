f689297e24f4f6eddd18c218e08c79a3e138477c
Amanda <acheng742@gmail.com>
2018-07-20 11:13:50 -0400

    rewrite class name to match format

--- src/components/ContactContent/index.js
+++ src/components/ContactContent/index.js
@@ -13,7 +13,7 @@ const ContactContent = () =>
       </div>
       <div className={ styles.summary }>
         <Hx color={ styles.offBlack } className={ styles.title } size="4">Check the FAQ</Hx>
-        <Para color={ styles.greyDark } className={ styles.subtitle }>Information about your membership, supported platforms, upgrading your plan, accessing labs &amp; exams, and more in the <a className={ styles['link'] } href="https://help.itpro.tv/" target="_blank" rel="noopener noreferrer">FAQ section</a>.</Para>
+        <Para color={ styles.greyDark } className={ styles.subtitle }>Information about your membership, supported platforms, upgrading your plan, accessing labs &amp; exams, and more in the <a className={ styles.link } href="https://help.itpro.tv/" target="_blank" rel="noopener noreferrer">FAQ section</a>.</Para>
       </div>
     </div>
     <div className={ `${styles.cols} ${styles.icon}` }>
@@ -22,7 +22,7 @@ const ContactContent = () =>
       </div>
       <div className={ styles.summary }>
         <Hx color={ styles.offBlack } className={ styles.title } size="4">Q&A Forum</Hx>
-        <Para color={ styles.greyDark } className={ styles.subtitle }>Interact with hosts and subject matter experts on technical questions about course content for your IT career in the <a className={ styles['link'] } href="https://forums.itpro.tv/" target="_blank" rel="noopener noreferrer">Q&amp;A Forum</a>.</Para>
+        <Para color={ styles.greyDark } className={ styles.subtitle }>Interact with hosts and subject matter experts on technical questions about course content for your IT career in the <a className={ styles.link } href="https://forums.itpro.tv/" target="_blank" rel="noopener noreferrer">Q&amp;A Forum</a>.</Para>
       </div>
     </div>
     <div className={ `${styles.cols} ${styles.icon}` }>
@@ -31,8 +31,8 @@ const ContactContent = () =>
       </div>
       <div className={ styles.summary }>
         <Hx color={ styles.offBlack } className={ styles.title } size="4">Reach out</Hx>
-        <Para color={ styles.greyDark } className={ styles.subtitle }>Phone, chat, and email support is available Monday - Thursday 7 a.m. - 7 p.m. Eastern and Friday 7 a.m. - 5 p.m. Eastern. Email us at <a className={ styles['email'] } href="mailto:support@itpro.tv">support@itpro.tv</a> or call us at
-          <a className={ styles['tel'] } href="tel:+13526006900"> +1-352-600-6900.</a>
+        <Para color={ styles.greyDark } className={ styles.subtitle }>Phone, chat, and email support is available Monday - Thursday 7 a.m. - 7 p.m. Eastern and Friday 7 a.m. - 5 p.m. Eastern. Email us at <a className={ styles.email } href="mailto:support@itpro.tv">support@itpro.tv</a> or call us at
+          <a className={ styles.tel } href="tel:+13526006900"> +1-352-600-6900.</a>
         </Para>
       </div>
     </div>
